fab7b73f8153e4e344bde74cbdf6586b4665d4ac
Ken Otero <keotero99@gmail.com>
2022-10-27 19:53:41 -0400

    Added Phaser script and some empty JS files.

---
+++ src/scene1.js
@@ -0,0 +1,26 @@
+/**
+ * Scene1 - Handles the logic for the game.
+ */
+class Scene1 {
+    /**
+     * preload() - Loads all assets needed for the game.
+     */
+    preload() {
+
+    }
+
+    /**
+     * create() - Gets called after preload(). This method initializes the scene.
+     */
+    create() {
+
+    }
+
+    /**
+     * update() - Gets called after create(). This method gets called 60 times
+     * per second and contains game logic.
+     */
+    update() {
+
+    }
+}
