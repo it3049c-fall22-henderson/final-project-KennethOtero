844745c6258c3578cad8a9a20b877299e90c816f
Ken Otero <keotero99@gmail.com>
2022-11-09 14:11:45 -0500

    Added a leaderboard object to game.js

--- src/game.js
+++ src/game.js
@@ -10,5 +10,15 @@ let config = {
     parent: "game"
 }
 
+// Leaderboard to contain users and their scores
+// Use Object.assign to add new players
+// (e.g., Object.assign(snakeLeaderboard, {player2: {name: "James", score: 0}});
+let snakeLeaderboard = {
+    player1: {
+        name: "Ken",
+        score: 0
+    }
+}
+
 // Create the game using our config
 // let game = new Phaser.Game(config);
