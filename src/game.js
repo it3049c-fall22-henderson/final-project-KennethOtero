1ac310028708632823cf9d222c0ad0e88c736e2e
Gabrielle E. McClimans <mcclimge@mail.uc.edu>
2022-12-06 12:21:09 -0500

    edited in High Score files for storage

--- src/game.js
+++ src/game.js
@@ -3,19 +3,49 @@ let config = {
     width: 700,
     height: 500,
     backgroundColor: 0x000000, // Black
-    scene: [Scene1, GameOver],
+    scene: Scene1,
     physics: {
         default: "arcade"
     },
     parent: "game"
 }
 
-// Leaderboard to contain users and their scores
-// Use Object.assign to add new players: key = name, value = score
-// (e.g., Object.assign(snakeLeaderboard, {Ken: 0});
-let snakeLeaderboard = {
-    Ken: 0
+if(High_Score <= score)
+{High_Score = score;}
+
+//High_Score = score;
+
+document.getElementById("").innerHTML = "Score = " + score;
+document.getElementById("").innerHTML = "High Score = " + High_Score;
+
+StoreInLocalStorage();
+
+function StoreInLocalStorage()
+{
+    localStorage.snake = JSON.stringify(snake);
+        localStorage.High_Score = JSON.stringify(High_Score);
 }
 
+function GetFromLocalStorage()
+{
+    if(!localStorage.snake)
+    {
+        localStorage.snake = JSON.stringify([]);
+        snake = {
+            x : 160,
+            y : 160 ,
+            cells : [{x:160 , y:160} , {x:144 , y:160}],
+        };
+    }
+}
+if (!localStorage.High_Score)
+{
+    localStorage.High_Score = JSON.stringify();
+}
+else
+{
+    High_Score = JSON.parse(localStorage.High_Score);
+}
 // Create the game using our config
 // let game = new Phaser.Game(config);
+
