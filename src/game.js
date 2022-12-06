d3afb15b576bd6f93b6c4b5e8cbe878e713985ac
Gabrielle E. McClimans <mcclimge@mail.uc.edu>
2022-12-06 15:01:22 -0500

    scoring changes continued

--- src/game.js
+++ src/game.js
@@ -10,42 +10,101 @@ let config = {
     parent: "game"
 }
 
-if(High_Score <= score)
-{High_Score = score;}
+// Read scores.json with Fetch API
 
-//High_Score = score;
+const Errors = document.getElementById("error");
+function get_scores (callback) {
+    let file = "scores.json";
+    fetch(file, {cache: 'no-cache'})
+    .then(function(response) {
+        //Response is not okay, then...
+        if (response.status !== 200) {
+            Errors.innerHTML = response.status;
+        }
+        //Response is good, then...
+        response.json().then(function (data){
+            let scores = JSON.stringify(data);
+            console.log(scores);
+            callback(scores);
+        });
+    });
+    //If theres an error, then...
 
-document.getElementById("").innerHTML = "Score = " + score;
-document.getElementById("").innerHTML = "High Score = " + High_Score;
-
-StoreInLocalStorage();
-
-function StoreInLocalStorage()
-{
-    localStorage.snake = JSON.stringify(snake);
-        localStorage.High_Score = JSON.stringify(High_Score);
-}
-
-function GetFromLocalStorage()
-{
-    if(!localStorage.snake)
-    {
-        localStorage.snake = JSON.stringify([]);
-        snake = {
-            x : 160,
-            y : 160 ,
-            cells : [{x:160 , y:160} , {x:144 , y:160}],
-        };
+    .catch(function (err) {
+        Errors.innerHTML = err;
     }
-}
-if (!localStorage.High_Score)
-{
-    localStorage.High_Score = JSON.stringify();
-}
-else
-{
-    High_Score = JSON.parse(localStorage.High_Score);
-}
-// Create the game using our config
-// let game = new Phaser.Game(config);
+  };
+
+
+//High Score list to be displayed
+
+//High Score List
+const List = document.getElementById("highscores");
+
+var list_scores = function (scores) {
+    let object = JSON.parse(scores);
+    //lower score saved for later
+
+    lowest_score = object[9].score;
+    document.getElementById("lowscore").value = lowest_score;
+    
+    //for Loop created here.
+    for (let i=0; i<object.length; i++) {
+        // console.log(object[i])
+        let li = document.createElement("LI");
+        let text = document.createTextNode(object[i].name + "..." + object[i].score);
+        li.appendChild(text);
+        List.appendChild(li);
+        if (i===0) {
+            li.setAttribute("class", "top1");
+        }
+        if (i===1) {
+            li.setAttribute("class", "top2");
+        }
+        if (i===2) {
+            li.setAttribute("class", "top2");
+             }
+         }
+    }
+ // reload items
+ function resetForm () {
+    while(list_scores.hasChildNodes()) {
+        List.removeChild(List.firstChild);
+    }
+    //fetch the data and make the High Score List
+    get_scores(list_scores);
+    //reset stuff
+    document.getElementById("score").value = 0;
+    score = 0;
+ }
+  //submit the form
+
+  //listen for clicking submit button
+  myform.addEventListener("submit", function (event) {
+    //dont reload page
+    event.preventDefault();
+
+    //lowest High Score
+    var tenth_score = document.getElementById("lowscore").value;
+
+    //form Data Object
+    var formData = new FormData (this);
+
+    //POST fetch requests
+    fetch ("snake.php", {
+        method: "post",
+        body: formData
+    })
+    .then (function (response) {
+        return response.text();
+    })
+    .then (function (text){
+        resetForm();
+        console.log(text);
+    })
+    .catch(function (err) {
+        Errors.innerHTML = err;
+    })
+  });
+
 
